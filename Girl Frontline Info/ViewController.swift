aa201a658d638bc5729c0a7121586fcc6d4cbb8b
Scarlet <info_ng@me.com>
2018-05-07 07:40:22 +0800

    Initial Commit

---
+++ Girl Frontline Info/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Girl Frontline Info
+//
+//  Created by Scarlet on 7/5/2018.
+//  Copyright © 2018 Scarlet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
