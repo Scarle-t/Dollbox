9f1f765e6329f9888493d2e056d0b8193c342f91
Scarlet <info_ng@me.com>
2018-09-12 04:07:04 +0800

    Clean Up Search

--- Girl Frontline Info/Controller/BuildtimeSearchCollectionViewController.swift
+++ Girl Frontline Info/Controller/BuildtimeSearchCollectionViewController.swift
@@ -9,35 +9,27 @@
 import UIKit
 
 class BuildtimeSearchCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIPickerViewDataSource, UIPickerViewDelegate ,getSearchProtocol {
+
+    let imgCache = Session.sharedInstance.imgSession
+    let pickerView = UIPickerView()
+    let searchResult = getSearchResult()
     
-    let hr = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21" ,"22", "23"]
-    let minAndSec = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21" ,"22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ,"32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"]
+    var feedItems : NSArray = NSArray()
+    var selectedTDoll: TDoll = TDoll()
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 3
     }
-    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
 
         if component == 0{
-            return hr.count
+            return 24
         }
-        
-        return minAndSec.count
-
-
+        return 60
     }
-    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        
-        if component == 0{
-            return hr[row]
-        }
-        
-        return minAndSec[row]
-
+        return String(row)
     }
-
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
         var pickerLabel: UILabel? = (view as? UILabel)
         if pickerLabel == nil {
@@ -45,91 +37,58 @@ class BuildtimeSearchCollectionViewController: UIViewController, UICollectionVie
             pickerLabel?.font = UIFont(name: "Mohave", size: 35)
             pickerLabel?.textAlignment = .center
         }
-        if component == 0{
-            pickerLabel?.text = hr[row]
-        }else{
-            pickerLabel?.text = minAndSec[row]
-        }
+        pickerLabel?.text = String(row)
         pickerLabel?.textColor = UIColor.black
         
         return pickerLabel!
     }
-    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
         switch component {
         case 0:
-            txtHour.text = hr[row]
+            txtHour.text = String(row)
         case 1:
-            txtMinute.text = minAndSec[row]
+            txtMinute.text = String(row)
         case 2:
-            txtSecond.text = minAndSec[row]
+            txtSecond.text = String(row)
         default:
             break
         }
-        
     }
-    
-    let imgCache = Session.sharedInstance.imgSession
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return feedItems.count
     }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        // Retrieve cell
         let cellIdentifier: String = "BasicCell"
         let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath as IndexPath) as! ResultCollectionViewCell
-        // Get the location to be shown
         let item: TDoll = feedItems[indexPath.row] as! TDoll
-        // Get references to labels of cell
-        
+
         if let photo_path = item.photo_path{
-            
             let urlString = URL(string: "https://scarletsc.net/girlfrontline/img/\(photo_path)")
-            
             let url = URL(string: "https://scarletsc.net/girlfrontline/img/\(photo_path)")
             
             if let imageFromCache = self.imgCache.object(forKey: url as AnyObject) as? UIImage{
-                
                 myCell.imgResult.image = imageFromCache
-                
             }else{
-                
                 DownloadPhoto().get(url: url!) { data, response, error in
                     guard let imgData = data, error == nil else { return }
                     print(url!)
                     print("Download Finished")
                     DispatchQueue.main.async(execute: { () -> Void in
-                        
-                        //tdoll.photo = UIImage(data: imgData)
-                        
                         let imgToCache = UIImage(data: imgData)
-                        
                         if urlString == url{
-                            
                             myCell.imgResult.image = imgToCache
-                            
                         }
-                        
                         if let imginCache = imgToCache{
                             self.imgCache.setObject(imginCache, forKey: urlString as AnyObject)
                         }
-                        
                     })
                 }
             }
         }
         
         myCell.lblResult.text = item.Zh_Name
-        
-        
-        
         return myCell
     }
-    
-
     func itemsDownloaded(items: NSArray) {
         feedItems = items
         self.collectionResult.reloadData()
@@ -138,51 +97,36 @@ class BuildtimeSearchCollectionViewController: UIViewController, UICollectionVie
     @IBAction func btnTimeSearch(_ sender: UIButton) {
         
         view.endEditing(true)
-        
-        let searchResult = getSearchResult()
-        searchResult.delegate = self
-        
         if let hour = txtHour.text,
             let minute = txtMinute.text,
             let second = txtSecond.text{
-            
             searchResult.urlPath = "https://scarletsc.net/girlfrontline/search.php?hour=\(hour)&minute=\(minute)&second=\(second)"
-            
         }
         searchResult.downloadItems()
     }
-    
     @IBOutlet weak var txtHour: UITextField!
     @IBOutlet weak var txtMinute: UITextField!
     @IBOutlet weak var txtSecond: UITextField!
-    
     @IBOutlet weak var collectionResult: UICollectionView!
-    
-    var feedItems : NSArray = NSArray()
-    
-    var selectedTDoll: TDoll = TDoll()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         
-        let pickerView = UIPickerView()
-        
         pickerView.delegate = self
         pickerView.dataSource = self
+        collectionResult.delegate = self
+        collectionResult.dataSource = self
+        searchResult.delegate = self
         
         txtSecond.inputView = pickerView
         txtMinute.inputView = pickerView
         txtHour.inputView = pickerView
         
-        collectionResult.delegate = self
-        collectionResult.dataSource = self
-        
         self.title = "製造時間搜尋"
         
-        navigationController?.navigationBar.barTintColor = UIColor.white
-        
+        setNavBarColor().white(self)
         tabBarController?.tabBar.barTintColor = UIColor.white
         
     }
@@ -191,12 +135,10 @@ class BuildtimeSearchCollectionViewController: UIViewController, UICollectionVie
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setNavBarColor().white(self)
     }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         let detailVC  = segue.destination as! TDollViewController
@@ -214,15 +156,5 @@ class BuildtimeSearchCollectionViewController: UIViewController, UICollectionVie
         }
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Girl Frontline Info/Controller/StarSearchViewController.swift
+++ Girl Frontline Info/Controller/StarSearchViewController.swift
@@ -10,97 +10,75 @@ import UIKit
 
 class StarSearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, getSearchProtocol {
     
+    //LET
     let imgCache = Session.sharedInstance.imgSession
-    
     let searchResult = getSearchResult()
     
+    //VAR
+    var feedItems: NSArray = NSArray()
+    var selectedTDoll: TDoll = TDoll()
+    
+    //DELEGATE
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return feedItems.count
     }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        // Retrieve cell
+
         let cellIdentifier: String = "BasicCell"
         let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath as IndexPath) as! ResultCollectionViewCell
-        // Get the location to be shown
         let item: TDoll = feedItems[indexPath.row] as! TDoll
-        // Get references to labels of cell
         
         if let photo_path = item.photo_path{
-            
             let urlString = URL(string: "https://scarletsc.net/girlfrontline/img/\(photo_path)")
-            
             let url = URL(string: "https://scarletsc.net/girlfrontline/img/\(photo_path)")
-            
             if let imageFromCache = self.imgCache.object(forKey: url as AnyObject) as? UIImage{
-                
                 myCell.imgResult.image = imageFromCache
-                
             }else{
-                
                 DownloadPhoto().get(url: url!) { data, response, error in
                     guard let imgData = data, error == nil else { return }
                     print(url!)
                     print("Download Finished")
                     DispatchQueue.main.async(execute: { () -> Void in
-                        
-                        //tdoll.photo = UIImage(data: imgData)
-                        
                         let imgToCache = UIImage(data: imgData)
-                        
                         if urlString == url{
-                            
                             myCell.imgResult.image = imgToCache
-                            
                         }
-                        
                         if let imginCache = imgToCache{
                             self.imgCache.setObject(imginCache, forKey: urlString as AnyObject)
                         }
-                        
                     })
                 }
             }
         }
         
         myCell.lblResult.text = item.Zh_Name
-        
         return myCell
-        
     }
-    
-    var feedItems: NSArray = NSArray()
-    
-    var selectedTDoll: TDoll = TDoll()
-    
     func itemsDownloaded(items: NSArray) {
         feedItems = items
         listResult.reloadData()
     }
     
+    //OUTLET
     @IBOutlet weak var listResult: UICollectionView!
-    
     @IBOutlet weak var navBarStars: UISegmentedControl!
-    
     @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
-        
         if navBarStars.selectedSegmentIndex != 0{
             navBarStars.selectedSegmentIndex = navBarStars.selectedSegmentIndex - 1
             swipeResult()
         }
-        
     }
-    
     @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer) {
-        
         if navBarStars.selectedSegmentIndex != 4{
             navBarStars.selectedSegmentIndex = navBarStars.selectedSegmentIndex + 1
             swipeResult()
         }
-        
+    }
+    @IBAction func navbBarStars(_ sender: UISegmentedControl) {
+        swipeResult()
     }
     
+    //FUNC
     func swipeResult(){
         
         switch navBarStars.selectedSegmentIndex {
@@ -125,23 +103,16 @@ class StarSearchViewController: UIViewController, UICollectionViewDataSource, UI
                 print(string)
             }
         }
-        
         searchResult.downloadItems()
-        
     }
-    
-    @IBAction func navbBarStars(_ sender: UISegmentedControl) {
-        swipeResult()
-    }
-    
+
+    //VIEW CONTROLLER
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         let detailVC  = segue.destination as! TDollViewController
         if let sender = sender as? UICollectionViewCell{
             let indexPath = self.listResult.indexPath(for: sender)
-            
             selectedTDoll = feedItems[(indexPath?.row)!] as! TDoll
-            
         }
         
         detailVC.selectedTDoll = self.selectedTDoll
@@ -150,44 +121,26 @@ class StarSearchViewController: UIViewController, UICollectionViewDataSource, UI
         }
         
     }
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
         listResult.delegate = self
         listResult.dataSource = self
+        searchResult.delegate = self
         
         navBarStars.setTitleTextAttributes([
             NSAttributedStringKey.font : UIFont(name: "Mohave", size: 17)!
             ], for: UIControlState.normal)
-        
         navBarStars.setTitleTextAttributes([
             NSAttributedStringKey.font : UIFont(name: "Mohave", size: 17)!
             ], for: UIControlState.selected)
         
-        searchResult.delegate = self
-        searchResult.urlPath = "https://scarletsc.net/girlfrontline/search.php?star=2"
-        
-        searchResult.downloadItems()
-        
         swipeResult()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
