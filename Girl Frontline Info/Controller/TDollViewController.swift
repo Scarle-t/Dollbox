319bc3d2a9833fdc4a9e900d99df054a3cac4bf5
Scarlet <info_ng@me.com>
2018-09-15 19:50:37 +0800

    Clean Up Doll Info

--- Girl Frontline Info/Controller/TDollViewController.swift
+++ Girl Frontline Info/Controller/TDollViewController.swift
@@ -11,12 +11,10 @@ import UIKit
 class TDollViewController: UIViewController {
     
     var selectedTDoll: TDoll?
-    
     var selectedImg: UIImage?
-    
     var from = ""
-    
     var imgCache = Session.sharedInstance.imgSession
+    var initialTouchPoint = CGPoint.zero
     
     @IBOutlet weak var Zh_name: UILabel!
     @IBOutlet weak var Eng_name: UILabel!
@@ -32,23 +30,17 @@ class TDollViewController: UIViewController {
     @IBOutlet weak var chain: UILabel!
     @IBOutlet weak var loads: UILabel!
     @IBOutlet weak var shield: UILabel!
-    
     @IBOutlet weak var critLabel: UILabel!
     @IBOutlet weak var chainLabel: UILabel!
     @IBOutlet weak var loadLabel: UILabel!
     @IBOutlet weak var shieldLabel: UILabel!
-    
     @IBOutlet weak var cover: UIImageView!
-    
     @IBOutlet weak var ammo: UILabel!
     @IBOutlet weak var mre: UILabel!
-    
     @IBOutlet weak var skill: UILabel!
     @IBOutlet weak var skill_desc: UITextView!
-    
     @IBOutlet weak var build_time: UILabel!
     @IBOutlet weak var obtain_method: UITextView!
-    
     @IBOutlet weak var area1: UIImageView!
     @IBOutlet weak var area2: UIImageView!
     @IBOutlet weak var area3: UIImageView!
@@ -58,22 +50,14 @@ class TDollViewController: UIViewController {
     @IBOutlet weak var area7: UIImageView!
     @IBOutlet weak var area8: UIImageView!
     @IBOutlet weak var area9: UIImageView!
-    
     @IBOutlet weak var buff: UITextView!
-    
     @IBOutlet weak var dismissBtn: UIButton!
-    
     @IBAction func dismiss(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    var initialTouchPoint = CGPoint.zero
-    
     @IBAction func panGestureRecognizerHandler(_ sender: UIPanGestureRecognizer) {
-        
         if dismissBtn.isHidden == false{
             let touchPoint = sender.location(in: view?.window)
-            
             switch sender.state {
             case .began:
                 initialTouchPoint = touchPoint
@@ -96,210 +80,19 @@ class TDollViewController: UIViewController {
                 break
             }
         }
- 
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
-        sepView.setEnabled(false, forSegmentAt: 3)
-        
-        if from == "stats"{
-            dismissBtn.isHidden = false
-        }
-        
-        sepView.setTitleTextAttributes([
-            NSAttributedStringKey.font : UIFont(name: "Mohave", size: 14)!
-            ], for: UIControlState.normal)
-        
-        sepView.setTitleTextAttributes([
-            NSAttributedStringKey.font : UIFont(name: "Mohave", size: 14)!
-            ], for: UIControlState.selected)
-        
-        Zh_name.text = selectedTDoll?.Zh_Name
-        Eng_name.text = selectedTDoll?.Eng_Name
-        type.text = selectedTDoll?.type
-        efficiency.text = selectedTDoll?.efficiency
-        health.text = selectedTDoll?.health
-        attack.text = selectedTDoll?.attack
-        speed.text = selectedTDoll?.speed
-        hitrate.text = selectedTDoll?.hit_rate
-        dodge.text = selectedTDoll?.dodge
-        movement_Speed.text = selectedTDoll?.movement
-        if let critRate = selectedTDoll?.critical{
-            if critRate == "0"{
-                critLabel.isHidden = true
-                crit_rate.isHidden = true
-            }else{
-                critLabel.isHidden = false
-                crit_rate.isHidden = false
-                crit_rate.text = critRate + "%"
-            }
-        }
-        
-        if selectedTDoll?.chain == "0" {
-            chainLabel.isHidden = true
-            chain.isHidden = true
-        }else{
-            chainLabel.isHidden = false
-            chain.isHidden = false
-            chain.text = selectedTDoll?.chain
-        }
-        
-        if selectedTDoll?.loads == "0" {
-            loadLabel.isHidden = true
-            loads.isHidden = true
-        }else{
-            loadLabel.isHidden = false
-            loads.isHidden = false
-            loads.text = selectedTDoll?.loads
-        }
-        
-        if selectedTDoll?.shield == "0" {
-            shieldLabel.isHidden = true
-            shield.isHidden = true
-        }else{
-            shieldLabel.isHidden = false
-            shield.isHidden = false
-            shield.text = selectedTDoll?.shield
-        }
-        
-        ammo.text = selectedTDoll?.ammo
-        mre.text = selectedTDoll?.mre
-        
-        skill.text = selectedTDoll?.skill_name
-        skill_desc.text = selectedTDoll?.skill_desc
-        
-        if let buildtime = selectedTDoll?.build_time{
-            if buildtime == "00:00:00"{
-                build_time.text = "無法製造"
-            }else{
-                build_time.text = buildtime
-            }
-        }
-        
-        obtain_method.text = selectedTDoll?.obtain_method
-        
-        cover.image = selectedImg
-        
-        switch selectedTDoll?.position{
-        case "1":
-            area1.image = #imageLiteral(resourceName: "position")
-        case "2":
-            area2.image = #imageLiteral(resourceName: "position")
-        case "3":
-            area3.image = #imageLiteral(resourceName: "position")
-        case "4":
-            area4.image = #imageLiteral(resourceName: "position")
-        case "5":
-            area5.image = #imageLiteral(resourceName: "position")
-        case "6":
-            area6.image = #imageLiteral(resourceName: "position")
-        case "7":
-            area7.image = #imageLiteral(resourceName: "position")
-        case "8":
-            area8.image = #imageLiteral(resourceName: "position")
-        case "9":
-            area9.image = #imageLiteral(resourceName: "position")
-        default:
-            break
-        }
-        
-        if selectedTDoll?.area1 == "1"{
-            area1.image = #imageLiteral(resourceName: "area")
-        }
-        
-        if selectedTDoll?.area2 == "1"{
-            area2.image = #imageLiteral(resourceName: "area")
-        }
-        
-        if selectedTDoll?.area3 == "1"{
-            area3.image = #imageLiteral(resourceName: "area")
-        }
-        
-        if selectedTDoll?.area4 == "1"{
-            area4.image = #imageLiteral(resourceName: "area")
-        }
-        
-        if selectedTDoll?.area5 == "1"{
-            area5.image = #imageLiteral(resourceName: "area")
-        }
-        
-        if selectedTDoll?.area6 == "1"{
-            area6.image = #imageLiteral(resourceName: "area")
-        }
-        
-        if selectedTDoll?.area7 == "1"{
-            area7.image = #imageLiteral(resourceName: "area")
-        }
-        
-        if selectedTDoll?.area8 == "1"{
-            area8.image = #imageLiteral(resourceName: "area")
-        }
-        
-        if selectedTDoll?.area9 == "1"{
-            area9.image = #imageLiteral(resourceName: "area")
-        }
-        
-        buff.text = selectedTDoll?.effect
-        
-        
-        if selectedTDoll?.stars == "2"{
-            setNavBarColor().white(self)
-        }
-        
-        if selectedTDoll?.stars == "3"{
-            setNavBarColor().blue(self)
-        }
-        
-        if selectedTDoll?.stars == "4"{
-            setNavBarColor().green(self)
-        }
-        
-        if selectedTDoll?.stars == "5"{
-            setNavBarColor().gold(self)
-        }
-        
-        if selectedTDoll?.stars == "EXTRA"{
-            setNavBarColor().purple(self)
-        }
-        
-        self.title = "人形資料"
-        
-    }
-    
     @IBOutlet weak var sepView: UISegmentedControl!
-    
     @IBOutlet weak var info: UIView!
     @IBOutlet weak var consum: UIView!
     @IBOutlet weak var skillInfo: UIView!
     @IBOutlet weak var buffInfo: UIView!
     @IBOutlet weak var method: UIView!
-    
-    @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
-        
-        if sepView.selectedSegmentIndex != 0{
-            sepView.selectedSegmentIndex = sepView.selectedSegmentIndex - 1
-            swipeView()
-        }
-        
+    @IBAction func sepInfo(_ sender: UISegmentedControl) {
+        swipeView()
     }
-    
-    @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer) {
-        
-        if sepView.selectedSegmentIndex != 4{
-            sepView.selectedSegmentIndex = sepView.selectedSegmentIndex + 1
-            swipeView()
-        }
-        
-    }
-    
     
     func swipeView(){
         switch sepView.selectedSegmentIndex{
-            
         case 0:
             info.isHidden = false
             consum.isHidden = true
@@ -340,27 +133,145 @@ class TDollViewController: UIViewController {
         }
     }
     
-    @IBAction func sepInfo(_ sender: UISegmentedControl) {
-    
-        swipeView()
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if from == "stats"{
+            dismissBtn.isHidden = false
+        }
+        sepView.setTitleTextAttributes([
+            NSAttributedStringKey.font : UIFont(name: "Mohave", size: 14)!
+            ], for: UIControlState.normal)
+        sepView.setTitleTextAttributes([
+            NSAttributedStringKey.font : UIFont(name: "Mohave", size: 14)!
+            ], for: UIControlState.selected)
+        Zh_name.text = selectedTDoll?.Zh_Name
+        Eng_name.text = selectedTDoll?.Eng_Name
+        type.text = selectedTDoll?.type
+        efficiency.text = selectedTDoll?.efficiency
+        health.text = selectedTDoll?.health
+        attack.text = selectedTDoll?.attack
+        speed.text = selectedTDoll?.speed
+        hitrate.text = selectedTDoll?.hit_rate
+        dodge.text = selectedTDoll?.dodge
+        movement_Speed.text = selectedTDoll?.movement
+        if let critRate = selectedTDoll?.critical{
+            if critRate == "0"{
+                critLabel.isHidden = true
+                crit_rate.isHidden = true
+            }else{
+                critLabel.isHidden = false
+                crit_rate.isHidden = false
+                crit_rate.text = critRate + "%"
+            }
+        }
+        if selectedTDoll?.chain == "0" {
+            chainLabel.isHidden = true
+            chain.isHidden = true
+        }else{
+            chainLabel.isHidden = false
+            chain.isHidden = false
+            chain.text = selectedTDoll?.chain
+        }
+        if selectedTDoll?.loads == "0" {
+            loadLabel.isHidden = true
+            loads.isHidden = true
+        }else{
+            loadLabel.isHidden = false
+            loads.isHidden = false
+            loads.text = selectedTDoll?.loads
+        }
+        if selectedTDoll?.shield == "0" {
+            shieldLabel.isHidden = true
+            shield.isHidden = true
+        }else{
+            shieldLabel.isHidden = false
+            shield.isHidden = false
+            shield.text = selectedTDoll?.shield
+        }
+        ammo.text = selectedTDoll?.ammo
+        mre.text = selectedTDoll?.mre
+        skill.text = selectedTDoll?.skill_name
+        skill_desc.text = selectedTDoll?.skill_desc
+        if let buildtime = selectedTDoll?.build_time{
+            if buildtime == "00:00:00"{
+                build_time.text = "無法製造"
+            }else{
+                build_time.text = buildtime
+            }
+        }
+        obtain_method.text = selectedTDoll?.obtain_method
+        cover.image = selectedImg
+        switch selectedTDoll?.position{
+        case "1":
+            area1.image = #imageLiteral(resourceName: "position")
+        case "2":
+            area2.image = #imageLiteral(resourceName: "position")
+        case "3":
+            area3.image = #imageLiteral(resourceName: "position")
+        case "4":
+            area4.image = #imageLiteral(resourceName: "position")
+        case "5":
+            area5.image = #imageLiteral(resourceName: "position")
+        case "6":
+            area6.image = #imageLiteral(resourceName: "position")
+        case "7":
+            area7.image = #imageLiteral(resourceName: "position")
+        case "8":
+            area8.image = #imageLiteral(resourceName: "position")
+        case "9":
+            area9.image = #imageLiteral(resourceName: "position")
+        default:
+            break
+        }
+        
+        if selectedTDoll?.area1 == "1"{
+            area1.image = #imageLiteral(resourceName: "area")
+        }
+        if selectedTDoll?.area2 == "1"{
+            area2.image = #imageLiteral(resourceName: "area")
+        }
+        if selectedTDoll?.area3 == "1"{
+            area3.image = #imageLiteral(resourceName: "area")
+        }
+        if selectedTDoll?.area4 == "1"{
+            area4.image = #imageLiteral(resourceName: "area")
+        }
+        if selectedTDoll?.area5 == "1"{
+            area5.image = #imageLiteral(resourceName: "area")
+        }
+        if selectedTDoll?.area6 == "1"{
+            area6.image = #imageLiteral(resourceName: "area")
+        }
+        if selectedTDoll?.area7 == "1"{
+            area7.image = #imageLiteral(resourceName: "area")
+        }
+        if selectedTDoll?.area8 == "1"{
+            area8.image = #imageLiteral(resourceName: "area")
+        }
+        if selectedTDoll?.area9 == "1"{
+            area9.image = #imageLiteral(resourceName: "area")
+        }
+        buff.text = selectedTDoll?.effect
+        
+        if selectedTDoll?.stars == "2"{
+            setNavBarColor().white(self)
+        }
+        if selectedTDoll?.stars == "3"{
+            setNavBarColor().blue(self)
+        }
+        if selectedTDoll?.stars == "4"{
+            setNavBarColor().green(self)
+        }
+        if selectedTDoll?.stars == "5"{
+            setNavBarColor().gold(self)
+        }
+        if selectedTDoll?.stars == "EXTRA"{
+            setNavBarColor().purple(self)
+        }
+        self.title = "人形資料"
     }
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
